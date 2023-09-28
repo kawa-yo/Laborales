e5c5a95af4a209083fd0a76b0296b21c72a35626
kawa-yo <yoshito.kawasaki.pub@gmail.com>
2023-09-29 06:00:15 +0900

    resolve dependency for sqflite and secureBookmarks

--- lib/launcher/project_builder/project_builder_model.dart
+++ lib/launcher/project_builder/project_builder_model.dart
@@ -1,15 +1,13 @@
-import 'dart:collection';
 import 'dart:io';
 
 import 'package:file_picker/file_picker.dart';
-import 'package:laborales/repository/secure_bookmarks.dart';
 
 Future<Directory?> pickDirectory() async {
   var path = await FilePicker.platform
       .getDirectoryPath(dialogTitle: "Pick the Directory");
   if (path != null) {
     var dir = Directory(path);
-    dir = await ensureToOpen(dir);
+    // dir = await ensureToOpen(dir);
     return dir;
   }
   return null;
